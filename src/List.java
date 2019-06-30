8f3dcdf00452083822f033c88f8536d33e34bc46
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-30 12:16:57 +0800

    新增 线性表抽象类 并实现 线性表接口中的共有方法

--- src/List.java
+++ src/List.java
@@ -18,7 +18,19 @@ public interface List {
     /**
      * 根据下表插入指定数字
      * @param index 给定的下标
-     * @param val 需要插入的数字
+     * @param val 需要插入的数字r
      */
-    void inset(int index, int val);
+    void insert(int index, int val);
+
+    /**
+     * 返回线性表数据的个数
+     * @return 线性表数据的个数
+     */
+    int size();
+
+    /**
+     * 返回当前线性表的迭代器
+     * @return 迭代器，从线性表最开始执行
+     */
+    Iterator iterator();
 }

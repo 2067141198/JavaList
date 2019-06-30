575dfcb1dd966f2fd78947234581a5bf265ec81b
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-30 13:31:07 +0800

    新增 顺序表类，并实现接口中所有的方法

---
+++ src/ArrayList.java
@@ -0,0 +1,70 @@
+public class ArrayList extends AbstractList implements List {
+    private int[] array;
+
+    ArrayList() {
+        this(16);
+    }
+
+    ArrayList(int val) {
+        array = new int[val];
+    }
+
+    /**
+     * 顺序表的迭代器
+     */
+    private class ArrayListIterator implements Iterator {
+        private int currentIndex;
+
+        private ArrayListIterator(int val) {
+            this.currentIndex = val;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < size();
+        }
+
+        @Override
+        public int next() {
+            return array[currentIndex++];
+        }
+    }
+
+    /**
+     *
+     * 根据下标插入要插入的数据
+     * @param index 要出入位置的下标
+     * @param val 要插入的数据
+     */
+    @Override
+    protected void insertInternal(int index, int val) {
+        ensureCapacity();
+        for(int i = size(); i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = val;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new ArrayListIterator(0);
+    }
+
+    /**
+     * 保证顺序表的容量是够用的
+     * 如果不够用，进行扩容，扩容为当前容量的2倍
+     */
+    private void ensureCapacity() {
+        if(size() < array.length) {
+            return;
+        }
+
+        int[] newArray = new int[size() * 2];
+
+        for(int i = 0; i < size(); i++) {
+            newArray[i] = array[i];
+        }
+
+        array = newArray;
+    }
+}

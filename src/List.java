e863d05370db7ef38c6113c261d7581953ff7e87
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-01 22:28:12 +0800

    补全线性表应该具有的 添加、删除和定位的接口

--- src/List.java
+++ src/List.java
@@ -23,6 +23,36 @@ public interface List {
     void insert(int index, int val);
 
     /**
+     * 将开头的元素删除
+     */
+    void popFront();
+
+    /**
+     * 将末尾的元素删除
+     */
+    void popBack();
+
+    /**
+     *删除给定下标位置处的元素
+     * @param index 要删除元素的下标
+     */
+    void earse(int index);
+
+    /**
+     * 返回给定下标位置处的值
+     * @param index 给定的下标
+     * @return index位置元素的值
+     */
+    int get(int index);
+
+    /**
+     * 将给定下标的元素的值进行修改
+     * @param index 给定的下标
+     * @param val 修改后的值
+     */
+    void set(int index, int val);
+
+    /**
      * 返回线性表数据的个数
      * @return 线性表数据的个数
      */
@@ -34,3 +64,4 @@ public interface List {
      */
     Iterator iterator();
 }
+

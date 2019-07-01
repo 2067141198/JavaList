28df3a51e56959ec650a3d20a81a12ce0f859a10
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-01 22:30:42 +0800

    新增 链表类的删除方法和根据下标获取数值即修改数值的方法

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,12 +1,20 @@
 public class LinkedList extends AbstractList implements List{
+    /**
+     * 内部类 创建节点类
+     */
     private class Node {
         int val;
         Node next;
 
-        Node(){
-            this.val = val;
+        Node(int val){
+            this(val, null);
         }
 
+        /**
+         *构造方法 根据传入的数据创建新结点
+         * 并使新结点的引用指向前驱结点所指向的结点
+         * 然后再使前驱结点的引用指向新建的结点
+         */
         Node(int val, Node next) {
             this.val = val;
             this.next = next;
@@ -15,6 +23,9 @@ public class LinkedList extends AbstractList implements List{
 
     private Node head = null;
 
+    /**
+     * 链表的迭代器
+     */
     private class LinkedListIterator implements Iterator {
         private Node cur = head;
 
@@ -31,17 +42,53 @@ public class LinkedList extends AbstractList implements List{
         }
     }
 
+
     @Override
     protected void insertInternal(int index, int val) {
         if(index == 0) {
             head = new Node(val, head);
         } else {
+            //先找 index 的前驱结点
             Node cur = head;
             for(int i = 0; i < index - 1; i++) {
                 cur = cur.next;
             }
             cur.next = new Node(val, cur.next);
         }
+    }
+
+    @Override
+    protected void earseInternal(int index) {
+
+        if(index == 0) {
+            head = null;
+        } else if(index == 1){
+            head.next = null;
+        } else {
+            Node cur = head;
+            for (int i = 0; i < index - 1; i++) {
+                cur = cur.next;
+            }
+            cur.next = cur.next.next;
+        }
+    }
+
+    @Override
+    public int get(int index) {
+        Node cur = head;
+        for(int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+
+    @Override
+    public void set(int index, int val) {
+        Node cur = head;
+        for(int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.val = val;
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,23 @@ public class Main {
         list.pushBack(10);
         list.pushBack(20);
         list.pushBack(30);
-        list.insert(3, 5);
+        list.insert(3, 5); //3 2 1 5 10 20 30
+        list.popFront();
+        list.popBack(); // 2 1 5 10 20
+        list.earse(2); //2 1 10 20
+        list.set(2, 6); // 2 1 6 20
 
         Iterator it = list.iterator();
 
         while(it.hasNext()) {
             System.out.println(it.next());
         }
+
+        System.out.println(list.get(3)); // 6
     }
     public static void main(String[] args) {
         testList(new ArrayList());
+        System.out.println("===");
+        testList(new LinkedList());
     }
 }
